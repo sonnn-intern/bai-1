c1f96d7d9513c0dcf9094aef4b2f4e64ab944262
Nguyễn Ngọc Sơn <sonnn_intern@flydinotech.com>
2025-07-03 12:59:19 +0700

    Add user information variables to index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+// định nghĩa các biến
+const userName = "Nguyễn Ngọc Sơn";
+const email = "sonnn@gmail.com";
+const phone = "0123456789";
+const address = "Đà nẵng, Việt Nam";
+const birthDay = "1990-01-01";
