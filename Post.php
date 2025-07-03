8f7936e8c719e0ccdc5f309bc541fbdb843de3e0
Nguyễn Ngọc Sơn <sonnn_intern@flydinotech.com>
2025-07-03 15:04:17 +0700

    Refactor showPost function parameters for improved readability

--- Post.php
+++ Post.php
@@ -5,7 +5,12 @@ $description = 'Bài 1: Giới thiệu về PHP';
 $author = 'Nguyễn Ngọc Sơn';
 $slug = 'bai-1-gioi-thieu-ve-php';
 
-function showPost($title, $description, $author, $slug) {
+function showPost(
+    $title,
+    $description,
+    $author,
+    $slug
+) {
     echo "<h1>$title</h1>";
     echo "<p>$description</p>";
     echo "<p><strong>Author:</strong> $author</p>";
