d7b86cb688c133e57c3b266534c99ef2bbfed8a9
Nguyễn Ngọc Sơn <sonnn_intern@flydinotech.com>
2025-07-03 14:49:57 +0700

    Create Post.php file

---
+++ Post.php
@@ -0,0 +1,15 @@
+<?php
+
+$title = 'Bài 1: Giới thiệu về PHP';
+$description = 'Bài 1: Giới thiệu về PHP';
+$author = 'Nguyễn Ngọc Sơn';
+$slug = 'bai-1-gioi-thieu-ve-php';
+
+function showPost($title, $description, $author, $slug) {
+    echo "<h1>$title</h1>";
+    echo "<p>$description</p>";
+    echo "<p><strong>Author:</strong> $author</p>";
+    echo "<p><strong>Slug:</strong> $slug</p>";
+}
+
+showPost($title, $description, $author, $slug);
